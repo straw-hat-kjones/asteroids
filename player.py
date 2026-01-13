bac4a803c937eb97aac5a41226a49b62bc28208e
straw-hat-kjones <keith@strawhatintel.com>
2026-01-12 18:08:06 -0800

    Rate-limited shot

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 from shot import Shot
 
 class Player(CircleShape):
@@ -8,6 +8,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.color = (0, 255, 0)  # Green color for the player
         self.rotation = 0  # Initial rotation angle
+        self.player_shot_cooldown = 0 
 
     # in the Player class
     def triangle(self):
@@ -25,6 +26,7 @@ class Player(CircleShape):
         return PLAYER_TURN_SPEED * dt
     
     def update(self, dt):
+        self.player_shot_cooldown -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -39,6 +41,7 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             return self.shoot()
+        
             
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
@@ -47,6 +50,9 @@ class Player(CircleShape):
         self.position += rotated_with_speed_vector
         
     def shoot(self):
+        if self.player_shot_cooldown > 0:
+            return None
+        self.player_shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
         shot = Shot(self.position.x, self.position.y)
         shot_direction = pygame.Vector2(0, 1).rotate(self.rotation)
         shot.velocity = shot_direction * PLAYER_SHOOT_SPEED
